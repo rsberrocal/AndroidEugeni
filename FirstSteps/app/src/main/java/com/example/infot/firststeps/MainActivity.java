9e0a20fcdbe8d6c3734b55c4d44cd82afa517b72
Richard Sudario Berrocal <rsberrocal@gmail.com>
2017-10-17 18:29:32 +0200

    Iniciado First Steps

--- FirstSteps/app/src/main/java/com/example/infot/firststeps/MainActivity.java
+++ FirstSteps/app/src/main/java/com/example/infot/firststeps/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.infot.firststeps2;
+package com.example.infot.firststeps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.infot.firststeps2.R;
 
 public class MainActivity extends AppCompatActivity {
 
