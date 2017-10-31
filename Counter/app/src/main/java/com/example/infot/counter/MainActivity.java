94d7fdb2a015380b92ab8ab35af9b266649b9830
Richard Sudario Berrocal <rsberrocal@gmail.com>
2017-10-31 20:00:33 +0100

    Añadidos funciones a los botones y añadido el boton reset

--- Counter/app/src/main/java/com/example/infot/counter/MainActivity.java
+++ Counter/app/src/main/java/com/example/infot/counter/MainActivity.java
@@ -4,12 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView countNum;
-    public CheckBox chBox;
+    public EditText countNum;
+    //public CheckBox chBox;
     public int num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +19,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addNum(View v){
-        this.countNum = (TextView)findViewById(R.id.txCount);
+        this.countNum = (EditText)findViewById(R.id.txCount);
         this.num++;
-        this.countNum.setText("Count :"+this.num);
+        System.out.println("qwmekq");
+        this.countNum.setText(String.valueOf(this.num));
     }
     public void removeNum(View v){
-        /*this.chBox=(CheckBox)findViewById(R.id.cbMin);
-        this.countNum = (TextView)findViewById(R.id.txCount);
-        if(this.chBox.isChecked()&&this.num <= 0){
+        //this.chBox=(CheckBox)findViewById(R.id.cbMin);
+        this.countNum = (EditText)findViewById(R.id.txCount);
+        //if(this.chBox.isChecked()&&this.num <= 0){
 
-        }else{
+        //}else{
             this.num--;
-            this.countNum.setText("Count :"+this.num);
-        }*/
+            this.countNum.setText(String.valueOf(this.num));
+        //}
     }
     public void resetNum(View v){
-        this.countNum = (TextView)findViewById(R.id.txCount);
+        this.countNum = (EditText)findViewById(R.id.txCount);
         this.num=0;
-        this.countNum.setText("Count :"+this.num);
+        this.countNum.setText(String.valueOf(this.num));
     }
 
 }
