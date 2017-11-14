5e5b7363caaf6b7cb51c411347ffec64459100ee
Richard Sudario Berrocal <rsberrocal@gmail.com>
2017-11-14 20:15:14 +0100

    Calculadora finalizada

--- Calculadora/app/src/main/java/cat/rsudario/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/cat/rsudario/calculadora/MainActivity.java
@@ -1,38 +1,120 @@
 package cat.rsudario.calculadora;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button btnOne;
-    public Button btnTwo;
-    public Button btnThree;
-    public Button btnFour;
-    public Button btnFive;
-    public Button btnSix;
-    public Button btnSeven;
-    public Button btnEight;
-    public Button btnNine;
-    public Button btnSum;
-    public Button btnMinus;
-    public Button btnMult;
-    public Button btnDiv;
+    public TextView txRes;
+    public TextView txPower;
+    public CheckBox cbPower;
+    public int firstNum;
+    public int secondNum;
+    public boolean firstOp=false;
+    public boolean stillOp=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.btnOne=(Button)findViewById(R.id.btnOne);
-        this.btnTwo=(Button)findViewById(R.id.btnTwo);
-        this.btnThree=(Button)findViewById(R.id.btnThree);
-        this.btnOne=(Button)findViewById(R.id.btnOne);
-        this.btnOne=(Button)findViewById(R.id.btnOne);
+        this.txRes=(TextView)findViewById(R.id.txResultat);
+        this.txPower=(TextView)findViewById(R.id.txPower);
+        this.cbPower=(CheckBox)findViewById(R.id.cbPower);
+        this.cbPower.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(cbPower.isChecked()){
+                    txPower.setText("On");
+                    txPower.setTextColor(Color.rgb(5, 173, 64));
+                }else{
+                    txPower.setText("Off");
+                    txPower.setTextColor(Color.rgb(206, 57, 16));
+                    txRes.setText("");
+                    firstOp=false;
+                    stillOp=true;
+                }
+
+            }
+        });
     }
 
-    public void addOne(View v){
-
+    public void addNum(View v){
+        Button btn = (Button)v;
+        if(this.cbPower.isChecked()){
+            if(!this.stillOp){
+                this.firstNum=Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText(btn.getText().toString());
+                this.stillOp=true;
+            }else{
+                this.txRes.setText(this.txRes.getText().toString()+btn.getText().toString());
+            }
+        }
     }
+
+    public void sum(View v){
+        if(this.cbPower.isChecked()){
+            if(!this.firstOp){
+                this.firstNum=Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText("");
+                this.firstOp=true;
+            }else{
+                this.secondNum=Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText(String.valueOf(this.firstNum+this.secondNum));
+                this.firstNum=this.secondNum;
+                this.stillOp=false;
+            }
+        }
+    }
+
+    public void min(View v){
+        if(this.cbPower.isChecked()) {
+            if(!this.firstOp){
+                this.firstNum=Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText("");
+                this.firstOp=true;
+            }else{
+                this.secondNum=Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText(String.valueOf(this.firstNum-this.secondNum));
+                this.firstNum=this.secondNum;
+                this.stillOp=false;
+            }
+        }
+    }
+
+    public void mult(View v){
+        if(this.cbPower.isChecked()) {
+            if (!this.firstOp) {
+                this.firstNum = Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText("");
+                this.firstOp = true;
+            } else {
+                this.secondNum = Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText(String.valueOf(this.firstNum * this.secondNum));
+                this.firstNum = this.secondNum;
+                this.stillOp = false;
+            }
+        }
+    }
+
+    public void div(View v){
+        if(this.cbPower.isChecked()) {
+            if (!this.firstOp) {
+                this.firstNum = Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText("");
+                this.firstOp = true;
+            } else {
+                this.secondNum = Integer.parseInt(this.txRes.getText().toString());
+                this.txRes.setText(String.valueOf(this.firstNum / this.secondNum));
+                this.firstNum = this.secondNum;
+                this.stillOp = false;
+            }
+        }
+    }
+
 }
